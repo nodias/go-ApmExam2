6d81ef15288c90d9cba031f9e6821dfdd68de1fe
kss <kss@antlinesoft.com>
2019-07-02 16:55:16 +0900

    govendor add again

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"go-ApmCommon/middleware"
 	"go-ApmCommon/model"
 	"go-ApmExam2/router"
-	"os"
 
 	"github.com/urfave/negroni"
 )
@@ -13,9 +12,6 @@ var config model.TomlConfig
 
 func init() {
 	config.Load()
-	//EXPORT APM EXVIRONMENT
-	os.Setenv("ELASTIC_APM_SERVER_URL", config.ApmServerUrl())
-	os.Setenv("ELASTIC_APM_SERVICE_NAME", config.Service)
 }
 func main() {
 	n := negroni.New(negroni.HandlerFunc(middleware.NewLoggingMiddleware(config.Logpaths.Logpath)))
