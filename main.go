4eaa1e95a66afe05e7f39363eea23f96c49ff459
kss <kss@antlinesoft.com>
2019-07-11 15:54:02 +0900

    refactoring toml, remove middleware

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"go-ApmCommon/models"
 	"go-ApmCommon/shared/logger"
-	"go-ApmCommon/shared/middleware"
 	"go-ApmExam2/api"
 	"go-ApmExam2/router"
 
@@ -18,11 +17,11 @@ func init() {
 	config = *models.GetConfig()
 	logger.Init()
 	api.Init()
-	log := logger.New(context.Background())
-	log.Info(config)
 }
 func main() {
-	n := negroni.New(negroni.HandlerFunc(middleware.Logging(config.Logconfig.Logpath)))
+	log := logger.New(context.Background())
+	n := negroni.New()
 	n.UseHandler(router.NewRouter())
+	log.Info("go-ApmExam2 - Server On!")
 	n.Run(config.Servers["ApmExam2"].PORT)
 }
